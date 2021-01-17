7698e8b40ad0e7974d7f627ff2fcf5d5f3df49bc
sametsolaklar <61630850+sametsolaklar@users.noreply.github.com>
2021-01-17 18:35:53 +0300

    Proje dosyası ekle.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 320000;
+            double faizOrani = 1.45;
+            bool sistemGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+        }
+    }
+}
